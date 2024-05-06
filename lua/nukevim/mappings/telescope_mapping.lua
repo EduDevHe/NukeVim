10c3c8530056080a955e08fc1489a02e697e31a7
EduDevHe <edu.he.dev@gmail.com>
2024-05-06 16:43:27 -0300

    feat: telescope mapping

---
+++ lua/nukevim/mappings/telescope_mapping.lua
@@ -0,0 +1,6 @@
+local map = vim.keymap.set
+
+map('n',"<leader>ff", "<cmd>Telescope find_files<cr>", {})
+map('n',"<leader>fg", "<cmd>Telescope live_grep<cr>", {})
+map('n',"<leader>fb", "<cmd>Telescope buffers<cr>", {})
+map('n',"<leader>fh", "<cmd>Telescope help_tags<cr>", {})
