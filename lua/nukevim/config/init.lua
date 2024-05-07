23162e06fc0744495923cf68a09d38e25a9b0391
EduDevHe <edu.he.dev@gmail.com>
2024-05-06 21:34:29 -0300

    feat options

---
+++ lua/nukevim/config/init.lua
@@ -0,0 +1,1 @@
+require("nukevim.config.options")
---
+++ lua/nukevim/config/options.lua
@@ -0,0 +1,39 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
+vim.opt.cursorline = true
+vim.opt.fillchars = { vert = "│" }
+vim.opt.laststatus = 3
+vim.opt.number = true
+vim.opt.scrolloff = 8
+vim.opt.shortmess:append("c")
+vim.opt.showmode = false
+vim.opt.sidescrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.termguicolors = true
+
+-- Indentation
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+
+-- Search
+vim.opt.grepprg = "rg --vimgrep"
+vim.opt.ignorecase = true
+vim.opt.inccommand = "split"
+vim.opt.smartcase = true
+
+-- Completion
+vim.opt.completeopt = { "menu", "noselect" }
+vim.opt.pumheight = 10
+
+-- Behavior
+vim.opt.clipboard = "unnamedplus"
+vim.opt.hidden = true
+vim.opt.undofile = true
+vim.opt.updatetime = 1000
+
