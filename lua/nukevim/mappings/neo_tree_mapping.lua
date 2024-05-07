89fdda3458f0f10a2c927a0ba694701a6ba3f1c4
EduDevHe <edu.he.dev@gmail.com>
2024-05-07 20:56:34 -0300

    feat: focus mapping

--- lua/nukevim/mappings/neo_tree_mapping.lua
+++ lua/nukevim/mappings/neo_tree_mapping.lua
@@ -1,4 +1,5 @@
 local map = vim.keymap.set
+local focus = vim.bo.filetype == 'neo-tree' and '<cmd>wincmd p<cr>' or '<cmd>Neotree focus<cr>'
 
 map('n',"<leader>e", "<CMD>Neotree toggle<CR>", {})
-
+map('n','<leader>o',focus,{}) 
