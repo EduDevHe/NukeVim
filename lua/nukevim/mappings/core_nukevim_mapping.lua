e7612f39e6fd1068ff09e302f797ecfb0c6ec3e4
EduDevHe <edu.he.dev@gmail.com>
2024-05-07 20:39:17 -0300

    feat: core nukevim mapping

---
+++ lua/nukevim/mappings/core_nukevim_mapping.lua
@@ -0,0 +1,15 @@
+local map = vim.keymap.set
+
+-- Quit
+map('n',"<leader>q", "<cmd>confirm q<cr>", {})
+map('n',"<leader>Q", "<cmd>confirm qall<cr>", {})
+
+map('n','<leader>ww', '<cmd>w<cr>', {desc = 'save'})
+
+
+
+-- Split navigation
+map('n','<C-H>', '<C-w>h', {})
+map('n','<C-J>', '<C-w>j', {})
+map('n','<C-K>', '<C-w>k', {})
+map('n','<C-L>', '<C-w>l', {})
--- lua/nukevim/mappings/init.lua
+++ lua/nukevim/mappings/init.lua
@@ -1,2 +1,3 @@
+require("nukevim.mappings.core_nukevim_mapping")
 require("nukevim.mappings.telescope_mapping")
 require("nukevim.mappings.neo_tree_mapping")
