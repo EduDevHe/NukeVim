aae3e2d2685373a5ae97b4f7428d85555af35758
EduDevHe <edu.he.dev@gmail.com>
2024-05-17 17:07:47 -0300

    feat: nvim-lspconfig config

---
+++ lua/nukevim/plugins/config/nvim-lspconfig.lua
@@ -0,0 +1,19 @@
+ local lspconfig = require("lspconfig")
+
+ lspconfig.tsserver.setup({
+        capabilities = capabilities
+})
+ 
+lspconfig.solargraph.setup({
+        capabilities = capabilities
+      })
+
+lspconfig.html.setup({
+        capabilities = capabilities
+})
+ 
+lspconfig.lua_ls.setup({
+        capabilities = capabilities
+})
+
+
