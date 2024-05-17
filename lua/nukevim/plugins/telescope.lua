d9c1661f7e473a14fed422e662e4e219234f8ab4
EduDevHe <edu.he.dev@gmail.com>
2024-05-17 13:59:46 -0300

    feat: telescope ui select

--- lua/nukevim/plugins/telescope.lua
+++ lua/nukevim/plugins/telescope.lua
@@ -1,8 +1,13 @@
  return {
-    "nvim-telescope/telescope.nvim",
-  
-    dependencies = {
+   { 
+     "nvim-telescope/telescope.nvim",
+      dependencies = {
       "nvim-lua/plenary.nvim",
-    }   
+    },
 
-  }
+  }, 
+  
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+  },
+}
