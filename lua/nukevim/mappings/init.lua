fa2a2f1064454fa444a22db32840d29e4cc94b23
EduDevHe <edu.he.dev@gmail.com>
2024-05-17 13:58:42 -0300

    feat: lsp mapping

--- lua/nukevim/mappings/init.lua
+++ lua/nukevim/mappings/init.lua
@@ -1,3 +1,4 @@
 require("nukevim.mappings.core_nukevim_mapping")
 require("nukevim.mappings.telescope_mapping")
 require("nukevim.mappings.neo_tree_mapping")
+require("nukevim.mappings.nvim-lspconfig")
