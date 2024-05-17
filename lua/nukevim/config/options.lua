1ec14486a16645392e1bbb1f69bb5b9d4238eef7
EduDevHe <edu.he.dev@gmail.com>
2024-05-17 19:11:07 -0300

    feat: loaded_netrwPlugin, loaded_netrw

--- lua/nukevim/config/options.lua
+++ lua/nukevim/config/options.lua
@@ -2,7 +2,8 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 vim.opt.swapfile = false
 vim.opt.fillchars:append { eob = " " }
-
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
 vim.opt.cursorline = true
 vim.opt.fillchars = { vert = "│" }
 vim.opt.laststatus = 3
