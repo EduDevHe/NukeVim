57c32594f54bba3969fccea729cdc5319a05803f
EduDevHe <edu.he.dev@gmail.com>
2024-05-18 16:35:57 -0300

    refactor: treessitter remove configs

---
+++ lua/nukevim/plugins/config/treesitter.lua
@@ -0,0 +1,12 @@
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = {},
+  sync_install = false,
+  auto_install = true,
+  indent = true,
+  ignore_install = {},
+  highlight = {
+    enable = true,
+    disable = {},
+    additional_vim_regex_highlighting = false,
+  },
+}
