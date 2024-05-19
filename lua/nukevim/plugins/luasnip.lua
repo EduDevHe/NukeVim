4df8518fa9c335c2e559cd7e4ec7137376f0581e
EduDevHe <edu.he.dev@gmail.com>
2024-05-19 00:03:58 -0300

    feat: dependencies

--- lua/nukevim/plugins/luasnip.lua
+++ lua/nukevim/plugins/luasnip.lua
@@ -4,4 +4,9 @@ return {
   version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
   -- install jsregexp (optional!).
   build = "make install_jsregexp",
+  dependece = {
+    "saadparwaiz1/cmp_luasnip",
+    "rafamadriz/friendly-snippets",
+  },
+  
 }
