abf7705565638d2352d501cdad1506454424050a
EduDevHe <edu.he.dev@gmail.com>
2024-05-19 00:04:29 -0300

    feat: lsp config

--- lua/nukevim/plugins/lsp.lua
+++ lua/nukevim/plugins/lsp.lua
@@ -3,7 +3,6 @@ return {
   lazy = false,
 
   config = function()
-    --     local capabilities = require('cmp_nvim_lsp').default_capabilities()
     require("nukevim.plugins.config.nvim-lspconfig")
   end,
 }
