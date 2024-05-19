05a7e59c49e95f0d71b4b11ca9f60a624f589c7c
EduDevHe <edu.he.dev@gmail.com>
2024-05-19 00:01:47 -0300

    feat: config neodev

--- lua/nukevim/plugins/neodev.lua
+++ lua/nukevim/plugins/neodev.lua
@@ -1,3 +1,14 @@
 return {
-  "folke/neodev.nvim",
+  {
+    "folke/neodev.nvim",
+    config = function()
+      require("nukevim.plugins.config.neodev")
+    end,
+  },
+  
+  {
+    "folke/neoconf.nvim",
+    config = function()
+    end,
+  },
 }
